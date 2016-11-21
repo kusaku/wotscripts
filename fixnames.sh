bd1962cafb23a645394bbca628d5dfcaa0ee2bb8
Kirill Arkhipenko <kirill.a@aggrostudios.com>
2016-11-21 14:29:54 +0300

    pre-0.9.17 merge, no changes

--- fixnames.sh
+++ fixnames.sh
@@ -2,27 +2,26 @@
 
 # bash script for filename case conversion
 # decompiled files have prefix:
-# Embedded file name: [type]/scripts/[original_pathname_here]
+# Embedded file name: scripts/[original_pathname_here]
 #
 # it tries to extract original names and rename
-# files moved from in~/[type]/script_in
-# to [type]/script directory
+# files moved from in~ to /script directory
 # extensions are changed to .py also
 
 
 fixname ()
 {
-    local TYPE=$1
+    local OUTDIR=$1
     local OLDFILE=$2
 
     # make new filename
-    local NEWFILE=$(head -n 1 $OLDFILE | sed -e "s/# Embedded file name: /$TYPE\//g")
+    local NEWFILE=$OUTDIR/$(head -n 1 $OLDFILE | sed -e "s/# Embedded file name: //g")
 
     # detect if it is correct
-    if [[ ! $NEWFILE =~ ^$TYPE/scripts/ ]];
+    if [[ ! $NEWFILE =~ ^$OUTDIR/scripts/ ]];
     then
         # fallback to default, just move in~/ -> .
-        NEWFILE=$(echo "${OLDFILE%.*}.py" | sed -e "s/in~\///g")
+        NEWFILE=$OUTDIR$(echo "${OLDFILE%.*}.py" | sed -e "s/in~\///g")
         echo "Bad header in $OLDFILE, using $NEWFILE"
     fi
 
@@ -39,16 +38,11 @@ fixname ()
 }
 
 
-find in~/ -name '*.pyc' -delete
+find in~ -name '*.pyc' -delete
 
-find in~/res -name '*.pyc_dis*' | while IFS=$'\n' read -r FILE;
+find in~ -name '*.pyc_dis*' | while IFS=$'\n' read -r FILE;
 do
-    fixname res $FILE
-done
-
-find in~/res_bw -name '*.pyc_dis*' | while IFS=$'\n' read -r FILE;
-do
-    fixname res_bw $FILE
+    fixname . $FILE
 done
 
 # wow!
